e09f33f1d1c919b0218205b463a8ed97fc343416
LCueto01 <renzo026@outlook.com>
2021-10-16 23:32:55 +0100

    added json stuff and improved dynamic item showing

---
+++ Test.js
@@ -0,0 +1,5 @@
+let doesStuff = () => {document.getElementsByClassName("para").innerHTML = "I have changed this lmao";}
+
+function doesStuff2(){
+    document.getElementsByClassName("para").innerHTML = "I have changed this lmao";
+}
